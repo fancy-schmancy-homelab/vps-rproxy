c711b1654a3220a7c8f2f9515996a8627cf4a4e9
Lumilias <10852161+Lumilias@users.noreply.github.com>
2025-08-10 10:26:12 -0500

    add public IP & NSG

--- az-terraform/main.tf
+++ az-terraform/main.tf
@@ -41,6 +41,56 @@ resource "azurerm_virtual_network" "vm_network" {
   resource_group_name = azurerm_resource_group.network_rg.name
 }
 
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "vm-nsg"
+  location            = azurerm_resource_group.network_rg.location
+  resource_group_name = azurerm_resource_group.network_rg.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    destination_port_range     = "22"
+    source_address_prefixes    = var.allowed_ip_addresses
+  }
+
+  security_rule {
+    name                       = "AllowICMP"
+    priority                   = 1010
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+  }
+
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 1020
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    destination_port_range     = "443"
+  }
+
+  security_rule {
+    name                       = "AllowHTTPS-UDP"
+    priority                   = 1030
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    destination_port_range     = "443"
+  }
+
+  security_rule = {
+    name                       = "Drop All Other Traffic"
+    priority                   = 4000
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+  }
+}
+
 resource "azurerm_subnet" "vm_subnet" {
   name                 = "vm-subnet"
   resource_group_name  = azurerm_resource_group.network_rg.name
@@ -49,6 +99,30 @@ resource "azurerm_subnet" "vm_subnet" {
   depends_on = [ azurerm_virtual_network.vm_network ]
   service_endpoints = ["Microsoft.KeyVault"]
 }
+
+resource "azurerm_subnet_network_security_group_association" "vm_subnet_nsg" {
+  subnet_id                 = azurerm_subnet.vm_subnet.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+  depends_on = [ azurerm_network_security_group.vm_nsg, azurerm_subnet.vm_subnet ]
+}
+
+resource "random_string" "random" {
+  length  = 16
+  special = false
+  upper   = false
+  lower   = true
+  numeric = true
+}
+
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "vps-rproxy-public-ip"
+  location            = azurerm_resource_group.network_rg.location
+  resource_group_name = azurerm_resource_group.network_rg.name
+  allocation_method   = "Dynamic"
+
+  domain_name_label   = "${random_string.random.result}" # Ensure this is unique across Azure
+}
+
 
 resource "azurerm_network_interface" "vm_nic" {
   name                = "vm-nic"
@@ -59,9 +133,11 @@ resource "azurerm_network_interface" "vm_nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.vm_subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
   }
-  depends_on = [ azurerm_subnet.vm_subnet ]
+  depends_on = [ azurerm_subnet.vm_subnet, azurerm_public_ip.vm_public_ip ]
 }
+
 
 resource "azurerm_key_vault" "kv" {
   name                        = "vps-rproxy-kv"
