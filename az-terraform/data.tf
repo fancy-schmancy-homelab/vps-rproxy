5b73a46a305f26c76b30b48f6c5ee62a1170cd51
Lumilias <10852161+Lumilias@users.noreply.github.com>
2025-08-10 00:00:10 -0500

    created disk encryption set outside of terraform due to permissions

--- az-terraform/data.tf
+++ az-terraform/data.tf
@@ -3,3 +3,8 @@ data "azuread_service_principal" "current" {
 }
 
 data "azurerm_subscription" "current" {}
+
+data "azurerm_disk_encryption_set" "vm_disk_encryption" {
+  name = "vm-disk-encryption-set"
+  resource_group_name = azurerm_resource_group.disk_encryption_rg.name
+}
